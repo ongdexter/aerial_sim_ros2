4ce47fef79481f39b488838fbe3cd4c7e9e08f1c
Dexter <dexterong94@gmail.com>
2025-08-26 14:47:23 -0400

    Added sim config

--- unity_interface/launch/sim.launch.py
+++ unity_interface/launch/sim.launch.py
@@ -1,9 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
+import os
 
 
 def generate_launch_description():
+    config_path = os.path.join(os.path.dirname(__file__), 'unity_sim_config.yaml')
     return LaunchDescription([
         Node(
             package='ros_tcp_endpoint',
@@ -12,7 +14,7 @@ def generate_launch_description():
             output='screen',
         ),
         ExecuteProcess(
-            cmd=['src/aerial_sim_ros2/aerial_sim_binaries/aerial_sim.x86_64'],
+            cmd=['src/aerial_sim_ros2/aerial_sim_binaries/aerial_sim.x86_64', '-config', config_path],
             output='screen'
         ),
-    ]) 
+    ])
